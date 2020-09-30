c0968c2df9c904695da802ce54d7e12c84ce6bda
Armin Basirian <ar.basirian@gmail.com>
2020-09-30 08:01:16 +0330

    Services

---
+++ src/services/giphy-api.js
@@ -0,0 +1,14 @@
+import Axios from "axios"
+
+// Get Gifs
+export const fetchGifList = async (query, limit = 25, offset = 0, rating = 'g', lang = 'en') => {
+    try {
+        return await Axios.get(
+            `https://api.giphy.com/v1/gifs/search?api_key=P4pWZwgvgYFxzcmQsqcaku65LWVUm2or&q=${query}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}`
+        ).then((result) => {
+            return result
+        });
+    } catch (error) {
+        return error;
+    }
+}

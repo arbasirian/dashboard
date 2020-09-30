0b4aef4a7a149faeb6b6590e16334067b6ac092e
Armin Basirian <ar.basirian@gmail.com>
2020-09-30 08:00:45 +0330

    Components

---
+++ src/components/login-form/login-form.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginForm = () => {
+    return (
+        <div className="login-form">
+            login form test here!
+        </div>
+    )
+}
+
+export default LoginForm;
---
+++ src/components/register-form/register-form.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './register-form.styles.scss';
+
+const RegisterForm = () => {
+    return (
+        <div className="register-from">
+            Register Form
+        </div>
+    )
+}
+
+export default RegisterForm

4c80a1a9b6626b3a9688a6dee984168a6b9f63fc
Armin Basirian <ar.basirian@gmail.com>
2020-09-30 08:00:33 +0330

    Public

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.scss';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Layout Components
+import AuthLayoutRoute from 'layouts/auth-layout/auth-layout-route.component';
+import DashboardLayoutRoute from 'layouts/dashboard/dashboard-layout-route.component';
+
+
+// Pages Component
+import AuthenticationPage from 'pages/authentication/authentication.component';
+import HomePage from 'pages/home/home.component';
+
+class App extends Component {
+  render() {
+    return(
+      <Router>  
+        <Switch>  
+          <Route exact path="/">  
+            <Redirect to="/auth/login" />  
+          </Route>  
+
+          {/* Auth Layout Pages */}
+          <AuthLayoutRoute path="/auth" component={AuthenticationPage}/>  
+
+          {/* Dashboard Layout Pages */}
+          <DashboardLayoutRoute path="/dashboard"  component={ HomePage }/>  
+
+
+        </Switch>  
+      </Router>  
+    )
+  }
 }
 
 export default App;
